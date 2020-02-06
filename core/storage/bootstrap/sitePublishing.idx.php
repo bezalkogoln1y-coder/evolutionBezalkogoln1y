7024353c876d580749e6cb578a3f4969ed5f79fc
homePC <muskatinartem@gmail.com>
2020-02-06 20:37:00 +0300

    First commit

---
+++ core/storage/bootstrap/sitePublishing.idx.php
@@ -0,0 +1,4 @@
+<?php
+$recent_update='1580998879';
+$cacheRefreshTime='0';
+
---
+++ manager/media/browser/mcpuk/tpl/tpl_css.php
@@ -0,0 +1,2 @@
+<link href="css.php?type=<?php echo $this->type ?><?php echo ($this->cms ? "&amp;cms={$this->cms}" : "" ) ?>" rel="stylesheet" type="text/css" />
+<link href="themes/<?php echo $this->config['theme'] ?>/style.css?ver=3" rel="stylesheet" type="text/css" />
